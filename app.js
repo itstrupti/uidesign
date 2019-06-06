4c8b1e544bb82cccad44f2dc846525aa77526ef4
itstrupti <48816456+itstrupti@users.noreply.github.com>
2019-06-06 19:22:14 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+var myview =new Vue({
+  el:"#our-app",
+  data:{
+    message:"this is our first code",
+    name:"tutorials",
+    title:"tutorial link",
+    ulink:"https://www.youtube.com/watch?v=I4kHTNOHkBY"
+  }
+});
